9e8dee31c6a2de3104df40509d9a6d1d198bae5d
Alokkodzz <alok63579@gmail.com>
2024-05-29 23:26:42 +0530

    Update DiskSize.sh
    Filed modified

--- DiskSize.sh
+++ DiskSize.sh
@@ -3,7 +3,7 @@ echo "check disk usage"
 disk_size='df -h|grep "/dev/root"|awk '{print $5}'|cut -d '%' -f1'
 echo "$disk_size% disk is filled"
 if [$disk_size -gt 40];
-the
+then
   echo "Disk utilization is 40%, extent volumme or delete files"
 else
   echo "Disk utilization is normal"
