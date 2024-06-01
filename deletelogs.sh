33e3beaac2b752c39bda928fb564c58e7abedfa4
Alokkodzz <alok63579@gmail.com>
2024-06-01 13:28:21 +0530

    Create deletelogs.sh
    To delete files older than 30 days

---
+++ deletelogs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "To delete files older than 30 days"
+path="$1"
+find $path -mtime +30
+If[ ($? -eq 0)];
+then
+  echo "FIles older than 30 days from path $path"
+else
+  echo "Deletion failed"
+fi
