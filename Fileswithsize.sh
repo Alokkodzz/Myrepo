43112fc6ea3825acbd1e3298cf3a09a1c6698b1e
Alokkodzz <alok63579@gmail.com>
2024-06-01 12:55:43 +0530

    Update Fileswithsize.sh
    Modified

--- Fileswithsize.sh
+++ Fileswithsize.sh
@@ -2,5 +2,5 @@
 echo "To list files in path with size"
 path="$1"
 du -ah $path|sort -hr|head -5>size.txt
-echo"These are the 5 5 file in $path"
+echo "These are the 5 5 file in $path"
 cat size.txt
