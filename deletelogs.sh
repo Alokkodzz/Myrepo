b1a30f2e1441d27a68cfb8818e30988a8c1d75c3
Alokkodzz <alok63579@gmail.com>
2024-06-05 12:45:40 +0530

    Update deletelogs.sh

--- deletelogs.sh
+++ deletelogs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "To delete files older than 30 days"
-path=`$1`
+path=$($1)
+echo $path
 find $path -mtime +30 -delete
-if ["$?" -ne 0]
+if [$? -eq 0]
    then
       echo "Failed!"
    else
