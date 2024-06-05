e6aa72e450e45460df512cb6532ad080be34e730
Alokkodzz <alok63579@gmail.com>
2024-06-05 12:51:30 +0530

    Update deletelogs.sh

--- deletelogs.sh
+++ deletelogs.sh
@@ -4,9 +4,9 @@ path="$1"
 echo $path
 find $path -mtime +30 -delete
 echo $?
-#if [$? -eq ]
-   #then
-      #echo "Failed!"
-   #else
-      #echo "Deleted!"
-   #fi
+if ["$?" == "0"]
+   then
+      echo "Failed!"
+   else
+      echo "Deleted!"
+   fi
